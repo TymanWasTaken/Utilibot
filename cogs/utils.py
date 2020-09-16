69c1ec71342a9ce89379b59fd29944658c86a7d0
Tyler Beckman <tbeckman530@gmail.com>
2020-09-16 12:12:54 -0600

    No message specified

--- cogs/utils.py
+++ cogs/utils.py
@@ -10,10 +10,10 @@ class Utils(commands.Cog):
 		color = "%06x" % random.randint(0, 0xFFFFFF)
 		embed = discord.Embed(title="Ping!", description=f"Pong! `{round(self.bot.latency * 1000)} ms`", color=int(color, 16))
 		await ctx.send(embed=embed)
-	
+
 	@commands.command()
-	@commands.has_permissions(administrator=True)
-	@commands.bot_has_permissions(administrator=True)
+	@commands.has_permissions(manage_messages=True)
+	@commands.bot_has_permissions(manage_messages=True)
 	async def purge(self, ctx, number):
 		deleted = await ctx.channel.purge(limit=int(number)+1)
 		message = await ctx.channel.send(f'Deleted {len(deleted)-1} message(s)')
