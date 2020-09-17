6fd8fa294ff54add2fbd9cf7ef6ee84bc6edc5f1
Tyler Beckman <tbeckman530@gmail.com>
2020-09-17 10:32:06 -0600

    No message specified

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,12 +2,23 @@ import discord, random, asyncio
 from discord.ext import commands
 
 # purge checks
-async def is_bot(m):
-	print(m.author.bot)
+def is_bot(m):
 	return 	m.author.bot
-async def is_not_bot(m):
-	print(not(m.author.bot))
+def is_not_bot(m):
 	return 	not(m.author.bot)
+async def purge_messages(number, channel, check=None):
+	if check is None:
+		return await channel.purge(limit=number)
+	diff_message = 0
+	total_message = 0
+	async for message in channel.history(limit=500):
+		if diff_message == number:
+			break
+		if check(message):
+			diff_message += 1
+		total_message += 1
+	return await channel.purge(limit=total_message, check=check)
+
 
 class Moderation(commands.Cog):
 	def __init__(self, bot):
@@ -16,24 +27,21 @@ class Moderation(commands.Cog):
 	@commands.command()
 	@commands.has_permissions(manage_messages=True)
 	@commands.bot_has_permissions(manage_messages=True)
-	async def purge(self, ctx, number, mode=None):
+	async def purge(self, ctx, number, mode="all"):
 		"""
 		Purge a specified amount of messages from the current channel.
 		"""
 		await ctx.message.delete()
 		mode = str(mode).lower()
 		number = int(number)
-		if mode is None:
-			deleted = await ctx.channel.purge(limit=number)
-			print('Deleting all messages')
+		if mode == "all":
+			deleted = await purge_messages(number, ctx.channel)
 		elif mode == "bot":
-			deleted = await ctx.channel.purge(limit=number, check=is_bot)
-			print('Deleting bot messages')
+			deleted = await purge_messages(number, ctx.channel, is_bot)
 		elif mode == "human":
-			deleted = await ctx.channel.purge(limit=number, check=is_not_bot)
-			print('Deleting human messages')
+			deleted = await purge_messages(number, ctx.channel, is_not_bot)
 		else:
-			return ctx.send('Mode must be one of: bot, human')
+			return await ctx.send('Mode must be one of: all (default), bot, or human')
 		message = await ctx.channel.send(f'Deleted {len(deleted)} message(s)')
 		await asyncio.sleep(2.5)
 		await message.delete()
