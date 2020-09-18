201280e8284f47c4a02dcf58c423244f325078de
Tyler Beckman <tbeckman530@gmail.com>
2020-09-18 13:27:42 -0600

    a

--- cogs/info.py
+++ cogs/info.py
@@ -4,6 +4,18 @@ from discord.ext import commands
 def randcolor():
 	return int("%06x" % random.randint(0, 0xFFFFFF), 16)
 
+def permsfromvalue(value):
+	perms = discord.Permissions(permissions=int(value))
+	perms_true = sorted([x for x,y in dict(perms).items() if y])
+	perms_false = sorted([x for x,y in dict(perms).items() if not y])
+	nice_perms = ""
+	perms_true = ["\u2705 `" + s for s in perms_true]
+	perms_false = ["\u274c `" + s for s in perms_false]
+	perms_combined = sorted(perms_true + perms_false, key=lambda x: x.strip('\u2705\u274c'))
+	for perm in perms_combined:
+		nice_perms = nice_perms + perm.replace("_", " ").capitalize() + "`\n"
+	return nice_perms
+
 class Info(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
@@ -24,7 +36,7 @@ class Info(commands.Cog):
 		embed = discord.Embed(title="Invite link", description="Click the links below to invite the bot to your server, or join our support server!\n[Click me to invite the bot!](https://discord.com/api/oauth2/authorize?client_id=755084857280954550&permissions=2147483639&redirect_uri=https%3A%2F%2Fdiscord.com&scope=bot)\n[Click me to join the support server!](https://discord.gg/Tbg3HDW)", color=randcolor())
 		await ctx.send(embed=embed)
 	
-	@commands.command(name="botperms")
+	@commands.command(name="botperms", aliases=['botpermissions'])
 	@commands.has_permissions(manage_guild=True)
 	async def _bot_permissions(self, ctx, channel_permissions="False"):
 		"""
@@ -33,18 +45,11 @@ class Info(commands.Cog):
 		channel_permissions = Whether or not to check the channel permissions, instead of the guild ones (default false)
 		"""
 		if channel_permissions.lower() == "true":
-			perms = sorted([x for x,y in dict(ctx.me.permissions_in(ctx.channel)).items() if y])
-			nice_perms = ""
-			for perm in perms:
-				nice_perms = nice_perms + "`" + perm.replace("_", " ").capitalize() + "`\n"
-			await ctx.send(embed=discord.Embed(title="All of the bot's permissions in this channel:", description=nice_perms, color=randcolor()))
+			await ctx.send(embed=discord.Embed(title="All of the bot's permissions in this channel (Permissions not able to be used in this type of channel will show as denied):", description=permsfromvalue(ctx.channel.permissions_for(ctx.me).value) + "\nRun `u!requiredperms` to see which ones the bot needs.", color=randcolor()))
 		elif channel_permissions.lower() == "false":
-			perms = sorted([x for x,y in dict(ctx.me.guild_permissions).items() if y])
-			nice_perms = ""
-			for perm in perms:
-				nice_perms = nice_perms + "`" + perm.replace("_", " ").capitalize() + "`\n"
-			await ctx.send(embed=discord.Embed(title="All of the bot's permissions in this server:", description=nice_perms, color=randcolor()))
+			await ctx.send(embed=discord.Embed(title="All of the bot's permissions in this server:", description=permsfromvalue(ctx.me.guild_permissions.value) + "\nRun `u!requiredperms` to see which ones the bot needs.", color=randcolor()))
 
 def setup(bot):
 	bot.add_cog(Info(bot))
+	commands.HelpCommand.cog = Info(bot)
 	print('[InfoCog] Info cog loaded')
--- cogs/utils.py
+++ cogs/utils.py
@@ -4,6 +4,18 @@ from discord.ext import commands
 def randcolor():
 	return int("%06x" % random.randint(0, 0xFFFFFF), 16)
 
+def permsfromvalue(value):
+	perms = discord.Permissions(permissions=int(value))
+	perms_true = sorted([x for x,y in dict(perms).items() if y])
+	perms_false = sorted([x for x,y in dict(perms).items() if not y])
+	nice_perms = ""
+	perms_true = ["\u2705 `" + s for s in perms_true]
+	perms_false = ["\u274c `" + s for s in perms_false]
+	perms_combined = sorted(perms_true + perms_false, key=lambda x: x.strip('\u2705\u274c'))
+	for perm in perms_combined:
+		nice_perms = nice_perms + perm.replace("_", " ").capitalize() + "`\n"
+	return nice_perms
+
 class Utils(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
@@ -32,21 +44,12 @@ class Utils(commands.Cog):
 		for channel in ctx.guild.channels:
 			await channel.set_permissions(role, **{permission: state})
 	
-	@commands.command(name="permissions")
+	@commands.command(name="permissions", aliases=['perms', 'permsvalue'])
 	async def permissions_from_value(self, ctx, value):
 		"""
 		Shows the permissions that correspond to a permissions value
 		"""
-		perms = discord.Permissions(permissions=int(value))
-		perms_true = sorted([x for x,y in dict(perms).items() if y])
-		perms_false = sorted([x for x,y in dict(perms).items() if not y])
-		nice_perms = ""
-		perms_true = ["\u2705 `" + s for s in perms_true]
-		perms_false = ["\u274c `" + s for s in perms_false]
-		perms_combined = sorted(perms_true + perms_false, key=lambda x: x.strip('\u2705\u274c'))
-		for perm in perms_combined:
-			nice_perms = nice_perms + perm.replace("_", " ").capitalize() + "`\n"
-		await ctx.send(embed=discord.Embed(title=f"Permissions for value {value}:", description=nice_perms, color=randcolor()))
+		await ctx.send(embed=discord.Embed(title=f"Permissions for value {value}:", description=permsfromvalue(value), color=randcolor()))
 
 def setup(bot):
 	bot.add_cog(Utils(bot))
