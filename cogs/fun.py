b6f42e5107ec443b72bd4b0d588e3a2e0d5f0eb9
Tyler Beckman <tbeckman530@gmail.com>
2020-09-21 16:31:45 -0600

    update paroot

--- cogs/fun.py
+++ cogs/fun.py
@@ -22,7 +22,7 @@ class Fun(commands.Cog):
 
 	@commands.command()
 	async def paroot(self, ctx):
-		await ctx.send("<:nou:755946318068973659>")
+		await ctx.send("<:paroot:755947816165048431>")
 def setup(bot):
 	bot.add_cog(Fun(bot))
 	print('[FunCog] Fun cog loaded')
