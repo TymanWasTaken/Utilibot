bcd10ac02d159323beced60bad98bd58ce3a3078
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-09 19:14:30 -0600

    fixing a var inconsistency

--- cogs/utils.py
+++ cogs/utils.py
@@ -56,7 +56,7 @@ class Utils(commands.Cog):
 		embed = discord.Embed(title=f"Members with the role __{role.name}__", color=color.value)
 		for member in role.members:
 			name = f"{name}\n• {member.name}#{member.discriminator})"
-		if len(members) > 2048:
+		if len(name) > 2048:
 			await ctx.send("List is to big to send.")
 		else:
 			embed.description=name
