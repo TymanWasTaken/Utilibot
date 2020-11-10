79d57ac57a6e3e5a03949691d34d4807a7e6dd12
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-09 19:13:35 -0600

    removing profile link from users in role

--- cogs/utils.py
+++ cogs/utils.py
@@ -55,7 +55,7 @@ class Utils(commands.Cog):
 		name = ""	
 		embed = discord.Embed(title=f"Members with the role __{role.name}__", color=color.value)
 		for member in role.members:
-			name = f"{name}\n• [{member.name}#{member.discriminator}](https://discord.com/users/{member.id})"
+			name = f"{name}\n• {member.name}#{member.discriminator})"
 		if len(members) > 2048:
 			await ctx.send("List is to big to send.")
 		else:
