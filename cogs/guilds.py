4f28ac5045a3f03eee603c0c11f0a2d789772ac4
Tyman <tbeckman530@gmail.com>
2020-11-10 20:56:28 -0500

    aa

--- cogs/guilds.py
+++ cogs/guilds.py
@@ -1,5 +1,12 @@
 import discord, random, datetime, asyncio, postbin, traceback
 from discord.ext import commands
+import concurrent.futures
+
+async def Average(bot, lst): 
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        result = await bot.loop.run_in_executor(
+            pool, lambda lst: sum(lst) / len(lst), lst)
+		
 
 class Guilds(commands.Cog):
 	def __init__(self, bot):
@@ -10,9 +17,11 @@ class Guilds(commands.Cog):
 	async def guilds(self, ctx):
 		guilds = len(self.bot.guilds)
 		users = len(self.bot.users)
+		avg = await Average(self.bot, [g.member_count for g in self.bot.guilds])
 		await ctx.send(embed=discord.Embed(title="Guild data", description=f"""
 		Guild count: {guilds}
 		Total user count: {users}
+		Average member count: {avg}
 		""".replace("	", "")
 		))
 
