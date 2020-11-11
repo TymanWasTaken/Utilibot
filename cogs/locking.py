ad022fb28d9cabbe8562a3781ed2bb12a3110c5d
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-10 23:42:28 -0600

    fixing indents

--- cogs/locking.py
+++ cogs/locking.py
@@ -75,11 +75,11 @@ class Locking(commands.Cog):
 			await ctx.send("Please provide 1 or more channels to add to the list!")
 		else:
 			if action == True:
-			#this will be for adding to the db'
-			pass
+				#this will be for adding to the db'
+				pass
 			else:
-			#this will be for removing from db
-			pass
+				#this will be for removing from db
+				pass
 		await ctx.send("lol that didn't do anything (yet) :joy:")
 
 
