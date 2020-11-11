c1f71842696a65be177d3a905920e4792167f644
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-11 12:58:09 -0600

    making shl/ushl owner only

--- cogs/locking.py
+++ cogs/locking.py
@@ -66,6 +66,7 @@ class Locking(commands.Cog):
 	@commands.bot_has_permissions(manage_channels=True)
 	@commands.has_permissions(manage_channels=True)
 	@commands.guild_only()
+	@commands.owner_only()
 	async def serverhardlockable(self, ctx, *channels: discord.TextChannel, action=bool):
 		"""
 		Adds (text) channels to the list of channels that can be affected by server hardlock/unhardlock.
@@ -86,6 +87,7 @@ class Locking(commands.Cog):
 	@commands.bot_has_permissions(manage_channels=True)
 	@commands.has_permissions(manage_channels=True, manage_guild=True)
 	@commands.guild_only()
+	@commands.owner_only()
 	async def serverhardlock(self, ctx, *, reason=f'None given.'):
 		"""
 		Locks the entire server by setting all channels' send messages permissions for @everyone to false.
@@ -108,6 +110,7 @@ class Locking(commands.Cog):
 	@commands.bot_has_permissions(manage_channels=True)
 	@commands.has_permissions(manage_channels=True, manage_guild=True)
 	@commands.guild_only()
+	@commands.owner_only()
 	async def serverunhardlock(self, ctx, *, reason='None given.'):
 		"""
 		Unlocks the entire server by setting all channels' send messages permissions for @everyone to neutral.
