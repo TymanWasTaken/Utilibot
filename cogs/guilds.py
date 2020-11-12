9d5c3ce719a5d73f54a113fa7094569e96a009f5
Tyman <tbeckman530@gmail.com>
2020-11-11 21:28:52 -0500

    botfarm detector

--- cogs/guilds.py
+++ cogs/guilds.py
@@ -1,14 +1,20 @@
-import discord, random, datetime, asyncio, postbin, traceback
+import discord, random, datetime, asyncio, postbin, traceback, re
 from discord.ext import commands
 import concurrent.futures
 
 def func(lst):
-		return sum(lst) / len(lst)
+	return sum(lst) / len(lst)
 
 async def Average(bot, l):
 	with concurrent.futures.ProcessPoolExecutor() as pool:
 		return await bot.loop.run_in_executor(pool, func, l)
-		
+
+def func2(num):
+	return round(num, 2)
+
+async def Round(bot, num):
+	with concurrent.futures.ProcessPoolExecutor() as pool:
+		return await bot.loop.run_in_executor(pool, func2, num)
 
 class Guilds(commands.Cog):
 	def __init__(self, bot):
@@ -38,6 +44,24 @@ class Guilds(commands.Cog):
 				tb = "".join(traceback.format_exception(type(error), error, error.__traceback__))
 				url = await postbin.postAsync(tb)
 				await ctx.send(f"An error occured while trying to leave that guild. Hastebin: {url}")
+
+	@guilds.command()
+	@commands.is_owner()
+	async def botfarms(self, ctx):
+		message = await ctx.send("Calculating...")
+		text = ""
+		for g in self.bot.guilds:
+			await g.chunk()
+			bots = []
+			for m in g.members:
+				if m.bot:
+					bots.append(m.id)
+			btm = (len(bots)/g.member_count)*100
+			btmround = await Round(self.bot, btm)
+			if btm >= 75:
+				text = text + f"**{g.name}**:\n- Bot %: `{btmround}%`\n- Bots/membercount: `{len(bots)}/{g.member_count}`\n- Guild ID: `{g.id}`\n\n"
+		await message.edit(content="", embed=discord.Embed(title="Bot farms detected:", description=text))
+
 
 def setup(bot):
 	bot.add_cog(Guilds(bot))
