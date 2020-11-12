93df40d7bc38217f9dc1c532e331932e614631e9
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-12 13:28:12 -0600

    fixing wipeinv more

--- cogs/utils.py
+++ cogs/utils.py
@@ -133,7 +133,7 @@ class Utils(commands.Cog):
 			else:
 				ignored = f"{ignored}, `{inv.code}`"
 		embed = discord.Embed(title=f"Bulk Deleted {ctx.guild}'s Invites!")
-		embed.add_field(name="Deleted:", value=deleted)
+		embed.add_field(name="Deleted:", value=(deleted or "None"))
 		embed.add_field(name="Ignored:", value=(ignored or "None"))
 		embed.add_field(name="Couldn't Delete:", value=(failed or "None"))
 		await ctx.send(embed=embed)
