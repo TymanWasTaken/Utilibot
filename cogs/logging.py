07ea7594c3c5ea9de6f47fa4b95ac8c07a38b095
Tyman <tbeckman530@gmail.com>
2020-11-14 16:31:39 -0500

    logs yeye

--- cogs/logging.py
+++ cogs/logging.py
@@ -12,6 +12,15 @@ class Logging(commands.Cog):
 		"""
 		await ctx.send("Logging coming soon!")
 
+	@commands.Cog.listener()
+	async def on_raw_message_edit(self, payload):
+		channel = self.bot.get_channel(payload.channel_id)
+		message = await channel.fetch_message(payload.message_id)
+		logchannel = discord.utils.get(channel.guild.text_channels, name="utilibot-logs")
+		if logchannel == None:
+			return
+		embed = discord.Embed(title="Message edited", description=f"Message edited in {channel.mention}:\n\nBefore:```{message.clean_content.replace('`', '​`​')}")
+
 def setup(bot):
 	bot.add_cog(Logging(bot))
 	print('[LoggingCog] Logging cog loaded')
