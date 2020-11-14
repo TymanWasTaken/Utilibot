b101250600b7a99b87a76f302ff3e74d29856d65
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-14 17:16:31 -0600

    fixed delete, successfully added purge?

--- cogs/logging.py
+++ cogs/logging.py
@@ -1,4 +1,4 @@
-import discord, dpytils
+import discord, dpytils, postbin
 from discord.ext import commands
 from datetime import datetime
 
@@ -32,9 +32,22 @@ class Logging(commands.Cog):
 		logchannel = discord.utils.get(message.guild.text_channels, name="utilibot-logs")
 		if logchannel == None:
 			return
-		embed=discord.Embed(title=f"Message Deleted in #{message.channel.name}", description=f"**Message:**```{message.clean_content.replace('`', '​`​')}", color=0xe41212, timestamp=datetime.now())
+		embed=discord.Embed(title=f"Message Deleted in #{message.channel.name}", description=f"```{message.clean_content.replace('`', '​`​')}```", color=0xe41212, timestamp=datetime.now())
 		embed.set_author(name=message.author, icon_url=message.author.avatar_url)
 		embed.set_footer(text=f"Author ID: {message.author.id}")
+		await logchannel.send(embed=embed)
+	
+	@commands.Cog.listener()
+	async def on_bulk_message_delete(self, messages):
+		logchannel = discord.utils.get(message.guild.text_channels, name="utilibot-logs")
+		if logchannel == None:
+			return
+		obj = messages[0]
+		post = f"{len(messages)} messages deleted in #{obj.channel.name}:\n"
+		for message in messages:
+			post = f"{post}\n\n{message.author.name} ({message.author.id}): {message.content}"
+		url = await postbin.postAsync(post)
+		embed=discord.Embed(title=f"{len(messages)} Messages Purged in {obj.channel.name}", description=f"View them here: {url}", color=0xe41212, timestamp=datetime.now()))
 		await logchannel.send(embed=embed)
 
 def setup(bot):
