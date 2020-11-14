590f6654c2ea39efcd664625fe7d08587614716e
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-14 16:41:30 -0600

    fixing deletion

--- cogs/logging.py
+++ cogs/logging.py
@@ -22,7 +22,7 @@ class Logging(commands.Cog):
 		logchannel = discord.utils.get(before.guild.text_channels, name="utilibot-logs")
 		if logchannel == None:
 			return
-		embed = discord.Embed(title=f"Message Edited in #{before.channel.name}", description=f"**Before:**```{before.clean_content.replace('`', '​`​')}```**After:**```{after.clean_content.replace('`', '​`​')}```Message link: [click here]({before.jump_url})", color=utils.randcolor(), timestamp=datetime.now())
+		embed = discord.Embed(title=f"Message Edited in #{before.channel.name}", description=f"**Before:**```{before.clean_content.replace('`', '​`​')}```**After:**```{after.clean_content.replace('`', '​`​')}```Message link: [click here]({before.jump_url})", color=0x1184ff, timestamp=datetime.now())
 		embed.set_author(name=before.author, icon_url=before.author.avatar_url)
 		embed.set_footer(text=f"Author ID: {before.author.id}")
 		await logchannel.send(embed=embed)
@@ -32,7 +32,7 @@ class Logging(commands.Cog):
 		logchannel = discord.utild.get(before.guild.text_channels, name="utilibot-logs")
 		if logchannel == None:
 			return
-		embed=discord.Embed(title=f"Message Deleted in #{message.channel.name}", description=f"**Message:**```{message.clean_content.replace('`', '​`​')}"
+		embed=discord.Embed(title=f"Message Deleted in #{message.channel.name}", description=f"**Message:**```{message.clean_content.replace('`', '​`​')}", color=0xe41212, timestamp=datetime.now())
 		embed.set_author(name=message.author, icon_url=message.author.avatar_url)
 		embed.set_footer(text=f"Author ID: {message.author.id}")
 		await logchannel.send(embed=embed)
