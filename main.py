a143922402aa4951b57bcc64e1f0d6a29a450bed
Tyman <=>
2020-11-16 15:41:55 -0500

    No message specified

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def on_command_error(ctx, error):
 @bot.event
 async def on_error(event, *args, **kwargs):
 	errorInfo = sys.exc_info()
-	tb = "".join(traceback.format_exception(errorInfo[0], errorInfo[1] errorInfo[2]))
+	tb = "".join(traceback.format_exception(errorInfo[0], errorInfo[1], errorInfo[2]))
 	await bot.get_channel(764333133738541056).send(f"""
 	{event} raised: ```py
 	{sys.exc_info()}```
