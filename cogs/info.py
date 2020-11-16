7f8c93fa9474bb277c1a7680bed19bd0e13f5776
Tyman <=>
2020-11-15 22:18:10 -0500

    forgot to send oops

--- cogs/info.py
+++ cogs/info.py
@@ -133,6 +133,7 @@ class Info(commands.Cog):
 				for cmd in sorted_commands[cmds]:
 					category_text = category_text + f"{cmd.name}: {cmd.brief}\n"
 				help_text = help_text + category_text + "\n"
+			await ctx.send(help_text)
 
 
 def setup(bot):
