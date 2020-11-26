9b2c84b2468ac96547d653d19fd3c96e2af3c4a2
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-26 12:49:50 -0600

    No message specified

--- main.py
+++ main.py
@@ -262,18 +262,14 @@ async def on_message(message):
 
 @bot.event
 async def on_command(ctx):
-	await ctx.send('yes sure i\'ll just not work')
-	if bot.get_command(ctx.command):
-		if not user.bot:
-			db = (await bot.dbquery("turkeyday", "notfirst", "userid=" + str(ctx.author.id)))
-			if not db[0][1] == 'true':
-				embed=discord.Embed(title=":turkey: Happy Turkey Day! :turkey:", description="To those of you in the United States, we wish you a happy Thanksgiving. What are you going to give thanks for today? We want to tell you: Thank *you* for choosing Utilibot! Have a great day!", color=0xcb611d)
-				embed.set_author(name="Special Thanksgiving Day message from the Utilibot Development Team")
-				embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
-	#			await bot.dbexec(f"INSERT INTO turkeyday VALUES ({str(ctx.author.id)}, 'true')")
-				await ctx.send(embed=embed)
-	else:
-		await ctx.send("lmao just testing error")
+	if not user.bot:
+		db = (await bot.dbquery("turkeyday", "notfirst", "userid=" + str(ctx.author.id)))
+		if not db[0][1] == 'true':
+			embed=discord.Embed(title=":turkey: Happy Turkey Day! :turkey:", description="To those of you in the United States, we wish you a happy Thanksgiving. What are you going to give thanks for today? We want to tell you: Thank *you* for choosing Utilibot! Have a great day!", color=0xcb611d)
+			embed.set_author(name="Special Thanksgiving Day message from the Utilibot Development Team")
+			embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
+#			await bot.dbexec(f"INSERT INTO turkeyday VALUES ({str(ctx.author.id)}, 'true')")
+			await ctx.send(embed=embed)
 
 
 @bot.event
