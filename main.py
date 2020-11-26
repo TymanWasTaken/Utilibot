52e97e78b3f439068752898e5f417b61a6422da1
Clari_7744 <isaac.vanarnem@gmail.com>
2020-11-26 13:09:58 -0600

    No message specified

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ async def on_command(ctx):
 	if not ctx.author.bot:
 		db = (await bot.dbquery("turkeyday", "notfirst", "userid=" + str(ctx.author.id)))
 		if not db:
-			embed=discord.Embed(title=":turkey: Happy Turkey Day! :turkey:", description="To those of you in the United States, we wish you a happy Thanksgiving. What are you going to give thanks for today? We want to tell you: Thank *you* for choosing Utilibot! Have a great day!", color=0xcb611d)
+			embed=discord.Embed(title=":turkey: Happy Turkey Day! :turkey:", description="To those of you in the United States, we wish you a happy Thanksgiving. What are you going to give thanks for today? We want to tell you: Thank *you* for choosing Utilibot! Have a great day!\n\n||This message will disappear at 12 AM (CST)||, color=0xcb611d)
 			embed.set_author(name="Special Thanksgiving Day message from the Utilibot Development Team")
 			embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
 			await bot.dbexec(f"INSERT INTO turkeyday VALUES ({str(ctx.author.id)}, 'true')")
