cef5e7ccc5c2d1e974aa9c421ebdb87c222f18d4
Clari <clarinet.puppy@gmail.com>
2020-12-01 22:59:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ async def on_message(message):
 			if db:
 				async with aiohttp.ClientSession() as s:
 					async with s.get("https://i.imgur.com/q3h9bED.png") as r:
-						await message.channel.send(f"{message.author.mention} has paid their respects.", file=discord.File(BytesIO(await r.content.read()), filename="press_f.png"))
+						await message.reply(f"{message.author.mention} has paid their respects.", file=discord.File(BytesIO(await r.content.read()), filename="press_f.png"))
 	afksearch=afkReg.search(message.content)
 	if afksearch:
 		try: user = message.guild.get_member(int(afksearch.group(1)))
