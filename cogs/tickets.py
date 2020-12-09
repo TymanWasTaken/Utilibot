502dc129ccc018704055ee898da85ea0f38098cd
Clari <clarinet.puppy@gmail.com>
2020-12-08 21:59:33 -0600

    init :)

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,1 +1,16 @@
-import discord\nfrom discord.ext import commands
+import discord
+from discord.ext import commands
+
+class Tickets(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+	
+	@commands.command(name="ticket", aliases=['tickets'], invoke_without_command=True)
+	@commands.guild_only()
+	async def ticket(self, ctx):
+		return "o.o tickets soon? :wink:"
+	
+def setup(bot):
+	bot.add_cog(Tickets(bot))
+	print('[TicketsCog] Tickets cog loaded!')
+	
