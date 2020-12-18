e2df3f7d1651fbfb87cd0ee454bce87d36c0f65c
Clari <clarinet.puppy@gmail.com>
2020-12-18 09:21:21 -0600

    Update debug.py

--- cogs/debug.py
+++ cogs/debug.py
@@ -138,11 +138,13 @@ class Debug(commands.Cog):
 	@commands.command()
 	@commands.is_owner()
 	async def botstatus(self, ctx, online: typing.Optional[discord.Status], *, newstatus=None):
+		online = online or ctx.guild.me.status
+		newstatus = str(newstatus)
 		if newstatus is None:
-			await self.bot.change_presence(status=(online if online else ctx.guild.me.status), activity=None)
+			await self.bot.change_presence(status=(online), activity=None)
 			await ctx.send(f"Removed my custom status and set my indicator to {self.bot.const_emojis[str(online)]} `{str(online).capitalize()}`")
 		else:
-			await self.bot.change_presence(status=(online if online else ctx.guild.me.status), activity=discord.Activity(type=discord.ActivityType.listening, name=newstatus))
+			await self.bot.change_presence(status=(online), activity=discord.Activity(type=discord.ActivityType.listening, name=newstatus))
 			await ctx.send(f"Changed my status to ```{newstatus}``` and set my indicator to {self.bot.const_emojis[str(online)]} `{str(online).capitalize()}`")
 
 	@commands.command()
