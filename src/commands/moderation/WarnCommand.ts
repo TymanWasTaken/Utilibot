1c4a5c35350b36ad307e3346244df937f7b541a7
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-03-18 11:49:19 -0600

    format

--- src/commands/moderation/WarnCommand.ts
+++ src/commands/moderation/WarnCommand.ts
@@ -1,7 +1,7 @@
-import { GuildMember } from "discord.js";
-import { BotCommand } from "../../lib/extensions/BotCommand";
-import { BotMessage } from "../../lib/extensions/BotMessage";
-import { Modlog, ModlogType } from "../../lib/types/Models";
+import { GuildMember } from 'discord.js';
+import { BotCommand } from '../../lib/extensions/BotCommand';
+import { BotMessage } from '../../lib/extensions/BotMessage';
+import { Modlog, ModlogType } from '../../lib/types/Models';
 
 export default class WarnCommand extends BotCommand {
 	public constructor() {
@@ -18,9 +18,12 @@ export default class WarnCommand extends BotCommand {
 					match: 'rest'
 				}
 			]
-		})
+		});
 	}
-	public async exec(message: BotMessage, { member, reason }: { member: GuildMember; reason: string }): Promise<void> {
+	public async exec(
+		message: BotMessage,
+		{ member, reason }: { member: GuildMember; reason: string }
+	): Promise<void> {
 		try {
 			const entry = Modlog.build({
 				user: member.id,
@@ -28,18 +31,24 @@ export default class WarnCommand extends BotCommand {
 				moderator: message.author.id,
 				type: ModlogType.WARN,
 				reason
-			})
+			});
 			await entry.save();
 		} catch (e) {
-			await message.util.send('Error saving to database, please contact the developers')
-			return
+			await message.util.send(
+				'Error saving to database, please contact the developers'
+			);
+			return;
 		}
 		try {
-			await member.send(`You were warned in ${message.guild.name} for reason "${reason}".`)
+			await member.send(
+				`You were warned in ${message.guild.name} for reason "${reason}".`
+			);
 		} catch (e) {
-			await message.util.send('Error messaging user, warning still saved.')
-			return
+			await message.util.send('Error messaging user, warning still saved.');
+			return;
 		}
-		await message.util.send(`${member.user.tag} was warned for reason "${reason}".`)
+		await message.util.send(
+			`${member.user.tag} was warned for reason "${reason}".`
+		);
 	}
 }
